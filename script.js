85485b5f152c1d849f61a9f2d0d48dd6bc965e62
KARL SAID B. GADONG <146708073+GadongKarlSaid@users.noreply.github.com>
2023-10-13 11:28:21 +0800

    feat: Activity 8 in DSA
    Array in JS

---
+++ script.js
@@ -0,0 +1,14 @@
+//Activity 8
+
+const favSingers = ["Taylor Swift", "New Jeans", "Bruno Mars"];
+const favNumbers = [10, 7, 14, 13];
+const mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log(favSingers);
+console.log(favNumbers);
+
+console.log(favSingers[0]);
+console.log(favSingers[1]);
+console.log(favSingers[2]);
+console.log(favNumbers[1]);
+console.log(mixedArr[3]);
